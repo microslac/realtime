8a2a51a18ea492ea0e02e53ec0abee55433e6c62
tanlda <ledinhanhtan.stack@gmail.com>
2024-04-02 01:15:21 +0700

    feat: wip - standardize

--- src/app/settings/rabbit.py
+++ src/app/settings/rabbit.py
@@ -2,11 +2,11 @@ from .base import EnvSettings, Field
 
 
 class RabbitSettings(EnvSettings):
-    protocol: str = Field(alias="QUEUE_PROTOCOL", default="amqp")
-    host: str = Field(alias="QUEUE_HOST", default="localhost")
-    port: int = Field(alias="QUEUE_PORT", default=5672)
-    username: str = Field(alias="QUEUE_USERNAME")
-    password: str = Field(alias="QUEUE_PASSWORD")
+    protocol: str = Field(alias="RABBITMQ_PROTOCOL", default="amqp")
+    host: str = Field(alias="RABBITMQ_HOST", default="localhost")
+    port: int = Field(alias="RABBITMQ_PORT", default=5672)
+    username: str = Field(alias="RABBITMQ_USERNAME")
+    password: str = Field(alias="RABBITMQ_PASSWORD")
 
     @property
     def url(self):
